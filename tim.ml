8f5ff2d1d89cef2a042bce9050e7e52bd0286787
Michal Kwiatkowski <michal@trivas.pl>
2016-09-10 12:56:05 +0200

    Pull `read_tim_record` function into `read_tim_file`, as that's the only place it's needed.

--- tim.ml
+++ tim.ml
@@ -27,14 +27,12 @@ let string_of_record_duration record =
   let open Time.Span in
   sprintf "%s - %s  [%s]" (format record.start) (format record.stop) (string_of_time_span diff)
 
-let read_tim_record json =
-  let open Yojson.Basic.Util in
-  let timestamp_of field = member field json |> to_int |> float |> Time.of_float in
-  {start = timestamp_of "start"; stop = timestamp_of "stop"}
-
 let read_tim_file file_name =
-  let json = Yojson.Basic.from_file file_name in
   let open Yojson.Basic.Util in
+  let read_tim_record json =
+    let timestamp_of field = member field json |> to_int |> float |> Time.of_float in
+    {start = timestamp_of "start"; stop = timestamp_of "stop"} in
+  let json = Yojson.Basic.from_file file_name in
   List.map (to_list json) ~f:read_tim_record
 
 let () =
