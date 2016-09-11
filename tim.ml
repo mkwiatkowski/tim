33a0461a3918272ee6f72542aa6cdf9bd61702c5
Michal Kwiatkowski <michal@trivas.pl>
2016-09-11 10:30:56 +0200

    Calculate totals for today, this week, this month and last month.
    Rename string_of_time_span and string_of_record_duration functions
    so they match their function better.

--- tim.ml
+++ tim.ml
@@ -14,17 +14,17 @@ let total_duration records =
   let open Time.Span in
   List.fold_left ~f:(fun s1 s2 -> s1 + s2) ~init:Time.Span.zero spans
 
-let string_of_time_span span =
+let string_of_duration span =
   let open Time.Span in
   let hours = int_of_float (to_hr span) in
   let minutes = (int_of_float (to_min span)) mod 60 in
   sprintf "%dh %2dmin" hours minutes
 
-let string_of_record_duration record =
+let string_of_record_timespan record =
   let format t = Time.format t "%H:%M:%S" ~zone:Time.Zone.local in
   let diff = time_span record in
   let open Time.Span in
-  sprintf "%s - %s  [%s]" (format record.start) (format record.stop) (string_of_time_span diff)
+  sprintf "%s - %s  [%s]" (format record.start) (format record.stop) (string_of_duration diff)
 
 let read_tim_file file_name =
   let open Yojson.Basic.Util in
@@ -34,9 +34,46 @@ let read_tim_file file_name =
   let json = Yojson.Basic.from_file file_name in
   List.map (to_list json) ~f:read_tim_record
 
-let () =
-  let ranges = read_tim_file "sample.json" in
-  let strings = List.map ranges ~f:string_of_record_duration in
-  let total = string_of_time_span (total_duration ranges) in
-  printf "Ranges:\n%s\n\nTotal: %s\n" (String.concat ~sep:"\n" strings) total
+let today =
+  Date.today ~zone:Time.Zone.local
 
+let to_date time =
+  Time.to_date time ~zone:Time.Zone.local
+
+let is_today time =
+  to_date time = today
+
+let same_year d1 d2 =
+  Date.year d1 = Date.year d2
+
+let is_this_week time =
+  let d = to_date time in
+  (same_year d today) && (Date.week_number d = Date.week_number today)
+
+let is_this_month time =
+  let d = to_date time in
+  (same_year d today) && (Date.month d = Date.month today)
+
+let is_last_month time =
+  let d = to_date time in
+  (same_year d today) && (Date.month d = Date.month (Date.add_months today (-1)))
+
+let filter_by_start_date records predicate =
+  List.filter records ~f:(fun r -> predicate r.start)
+
+let summary records =
+  let string_total predicate =
+    string_of_duration (total_duration (filter_by_start_date records predicate)) in
+  let today_timespans =
+    let records = filter_by_start_date records is_today in
+    let strings = List.map records ~f:string_of_record_timespan in
+    String.concat ~sep:"\n" strings in
+  let today_total = string_total is_today in
+  let this_week_total = string_total is_this_week in
+  let this_month_total = string_total is_this_month in
+  let last_month_total = string_total is_last_month in
+  sprintf "Today:\n%s\n\nTotal: %s\nThis week: %s. This month: %s. Last month: %s.\n" today_timespans today_total this_week_total this_month_total last_month_total
+
+let () =
+  let records = read_tim_file "sample.json" in
+  printf "%s" (summary records)
