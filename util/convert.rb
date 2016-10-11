04cee79503b1ec8e5d7bf63cb7ab97508f07bd33
Michal Kwiatkowski <michal@trivas.pl>
2016-10-11 18:39:02 +0200

    Add a converter script from timert file format to tim format.

---
+++ util/convert.rb
@@ -0,0 +1,11 @@
+# Converter of timert format to tim format.
+#
+# Usage:
+#   ruby convert.rb TIMERT_JSON_FILE_PATH > output.json
+
+require 'json'
+
+puts JSON.load(File.open(ARGV.first, 'r')).
+  flat_map {|_, day| day['intervals']}.
+  select {|record| record['start'] && record['stop']}.
+  to_json
