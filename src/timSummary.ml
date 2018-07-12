6c0b1d95d41ebbe0e1737ffc8398c45b6f96ebbd
Michal Kwiatkowski <michal@trivas.pl>
2018-07-12 11:42:34 +0200

    Change summary format to use a table with days as rows and projects as columns.
    Use `|>` operator more for better readability.

--- src/timSummary.ml
+++ src/timSummary.ml
@@ -39,64 +39,111 @@ let filter_by_start_date records predicate =
 let filter_by_project records project =
   List.filter records ~f:(fun r -> r.TimRecord.project = project)
 
+let project_total records project =
+  total_duration (filter_by_project records project)
+
 let join_with_nl strings =
-  String.concat (List.map strings ~f:(fun s -> s ^ "\n"))
+  strings
+  |> List.map ~f:(fun s -> s ^ "\n")
+  |> String.concat
 
 let string_total records =
-  string_of_duration (total_duration records)
+  records
+  |> total_duration
+  |> string_of_duration
 
 let percentage records goal =
   int_of_float ((total_duration records |> Time.Span.to_hr) *. 100.0 /. (float goal))
 
 let summary records daily_hours_goal =
   let open TimDate in
-  let today_records = filter_by_start_date records is_today in
-  let this_week_records = filter_by_start_date records is_this_week in
-  let this_month_records = filter_by_start_date records is_this_month in
-  let last_month_records = filter_by_start_date records is_last_month in
+  let today_records =
+    filter_by_start_date records is_today in
+  let this_week_records =
+    filter_by_start_date records is_this_week in
+  let this_month_records =
+    filter_by_start_date records is_this_month in
+  let last_month_records =
+    filter_by_start_date records is_last_month in
   let today_timespans =
-    join_with_nl (List.sort ~compare:compare (List.map today_records ~f:string_of_record_timespan)) in
-  let this_month_timespans =
-    let total_of_day day =
-      total_duration (filter_by_start_date this_month_records (fun t -> day = (to_date t))) in
+    today_records
+    |> List.map ~f:string_of_record_timespan
+    |> List.sort ~compare:compare
+    |> join_with_nl in
+  let recent_projects =
+    last_month_records @ this_month_records
+    |> List.map ~f:(fun r -> r.TimRecord.project)
+    |> List.sort ~compare:compare
+    |> List.stable_dedup in
+  let this_month_goal =
+    this_month_work_days_number * daily_hours_goal in
+  let this_month_expected_so_far =
+    this_month_work_days_so_far * daily_hours_goal |> float_of_int |> Time.Span.of_hr in
+  let this_month_goal_difference =
+    let open Time.Span in
+    (total_duration this_month_records) - this_month_expected_so_far in
+  let this_week_goal =
+    5 * daily_hours_goal in
+  let expand =
+    sprintf "%10s" in
+  let this_month_status =
+    let open Time.Span in
+    let is_ahead = this_month_goal_difference > Time.Span.zero in
+    sprintf "%s %s"
+            (in_color (if is_ahead then ANSITerminal.green else ANSITerminal.red)
+                      (Time.Span.abs this_month_goal_difference |> string_of_duration))
+            (if is_ahead then "ahead" else "behind") in
+  let column_per_project separator f =
+    recent_projects
+    |> List.map ~f:f
+    |> List.intersperse ~sep:separator
+    |> String.concat in
+  let title_row =
+    sprintf "|            | %s |      TOTAL |" (column_per_project " | " expand) in
+  let separator_row =
+    sprintf "+------------+-%s-+------------+" (column_per_project "-+-" (fun _ -> "----------")) in
+  let row ?(color=ANSITerminal.default) label records =
+    let project_duration_string p =
+      let total = project_total records p in
+      if total = Time.Span.zero then
+        "         -"
+      else
+        string_of_duration total |> expand |> in_color color in
+    sprintf "| %10s | %s | %10s |"
+            label
+            (column_per_project " | " project_duration_string)
+            (string_total records |> expand |> in_color color) in
+  let this_month_day_rows =
+    let records_of_day day =
+      filter_by_start_date this_month_records (fun t -> day = (to_date t)) in
     let total_not_zero day =
-      (total_of_day day) <> Time.Span.zero in
-    let string_of_day day =
-      sprintf "%s: %s" (in_color ANSITerminal.cyan (Date.to_string day)) (total_of_day day |> string_of_duration) in
-    join_with_nl (List.map (List.filter ~f:total_not_zero this_month_days) ~f:string_of_day) in
-  let per_project_timespans =
-    let projects_this_month =
-      List.stable_dedup (List.sort ~compare:compare (List.map this_month_records ~f:(fun r -> r.TimRecord.project))) in
-    let total_of_project p =
-      total_duration (filter_by_project this_month_records p) in
-    let string_of_project p =
-      sprintf "%s: %s" (in_color ANSITerminal.magenta p) (total_of_project p |> string_of_duration) in
-    join_with_nl (List.map projects_this_month ~f:string_of_project) in
-  let today_total = string_total today_records in
-  let this_week_total = string_total this_week_records in
-  let this_month_total = string_total this_month_records in
-  let last_month_total = string_total last_month_records in
-  let this_month_goal = TimDate.this_month_work_days_number * daily_hours_goal in
-  let open Time.Span in
-  let this_month_expected_so_far = float_of_int (TimDate.this_month_work_days_so_far * daily_hours_goal) |> of_hr in
-  let this_month_goal_difference = (total_duration this_month_records) - this_month_expected_so_far in
-  let this_week_goal = 5 * daily_hours_goal in
-  sprintf "%s\n%s\n%s %s. %s %s (%d%% goal).\n\n%s\n%s\n%s\n%s\n%s %s (%d%% goal, %s %s). %s %s.\n"
-          (in_color ANSITerminal.green "Today:")
-          today_timespans
-          (in_color ANSITerminal.green "Total:")
-          today_total
-          (in_color ANSITerminal.green "This week:")
-          this_week_total
-          (percentage this_week_records this_week_goal)
-          (in_color ANSITerminal.green "This month:")
-          this_month_timespans
-          (in_color ANSITerminal.green "Per project:")
-          per_project_timespans
-          (in_color ANSITerminal.green "Total:")
-          this_month_total
-          (percentage this_month_records this_month_goal)
-          (string_of_duration (Time.Span.abs this_month_goal_difference))
-          (if this_month_goal_difference > Time.Span.zero then "ahead" else "behind")
-          (in_color ANSITerminal.green "Last month:")
-          last_month_total
+      (day |> records_of_day |> total_duration) <> Time.Span.zero in
+    let row_of_day day =
+      row (Date.to_string day) (records_of_day day) in
+    this_month_days
+    |> List.filter ~f:((<>) today)
+    |> List.filter ~f:total_not_zero
+    |> List.map ~f:row_of_day in
+  let table =
+    join_with_nl
+      ([separator_row;
+       title_row;
+       separator_row;
+       row "Last month" last_month_records;
+       separator_row]
+       @ this_month_day_rows
+       @ [row "Today" today_records;
+          separator_row;
+          row "This week" this_week_records;
+          separator_row;
+          row "This month" this_month_records ~color:ANSITerminal.Bold;
+          separator_row]) in
+  let today =
+    sprintf "%s\n%s" (in_color ANSITerminal.green "Today:") today_timespans in
+  let goals =
+    sprintf "This week %s goal. This month %s goal, %s.\n"
+            (percentage this_week_records this_week_goal |> sprintf "%d%%" |> in_color ANSITerminal.Bold)
+            (percentage this_month_records this_month_goal |> sprintf "%d%%" |> in_color ANSITerminal.Bold)
+            this_month_status
+  in
+  sprintf "%s\n%s\n%s" table today goals
